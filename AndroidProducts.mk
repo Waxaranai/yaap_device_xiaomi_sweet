cc4e375646ccef1913be7185ea9301eb208b5792
Waxaranai <waxaranai@gmail.com>
2023-02-27 13:16:23 +0000

    sweet: YAAP Bringup * Drop livedisplay * Set 2nd arch to armv8-2a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sweet.mk
+    $(LOCAL_DIR)/yaap_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_sweet-user \
-    aosp_sweet-userdebug \
-    aosp_sweet-eng
+    yaap_sweet-user \
+    yaap_sweet-userdebug \
+    yaap_sweet-eng
